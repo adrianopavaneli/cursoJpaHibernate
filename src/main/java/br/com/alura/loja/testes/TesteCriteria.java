f2ec11b4dad212a3b85bedaaa37a6a9d689002ad
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-30 21:40:14 -0300

    mapeamento com heranca de classes

--- src/main/java/br/com/alura/loja/testes/TesteCriteria.java
+++ src/main/java/br/com/alura/loja/testes/TesteCriteria.java
@@ -1,20 +1,21 @@
 package br.com.alura.loja.testes;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class TesteCriteria {
 
 	public static void main(String[] args) {
 		EntityManager em = JPAUtil.getEntityManager();
-		
 		ProdutoDao produtoDao = new ProdutoDao(em);
-		produtoDao.buscarPorParametros(null, null, LocalDate.now());
-		
+		List<Produto> produtos = produtoDao.buscarPorParametrosComCriteria(null, null, LocalDate.now());
+		produtos.forEach(p -> System.out.println(p.getNome()));
 	}
 
 }
